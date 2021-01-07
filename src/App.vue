101f114e89a6f9bb392c76da7b009aa9d30e54ba
syntappz <doddsanater87@gmail.com>
2021-01-07 17:21:22 +0000

    welcome screen

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Audiowide&family=Goldman&family=Yeon+Sung&display=swap");
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 16px;
 }
 
 
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,17 +1,75 @@
 <template>
-  <h1>{{title}}</h1>
+  <div class="welcome">
+    <img class="background-image" :src="image2" alt="mountains-image" />
+
+    <h1 class="title">{{ title }}</h1>
+    <div class="screens-wrap">
+      <img class="mobile-screens" :src="screens" alt="screens" />
+    </div>
+  </div>
 </template>
 
 <script>
+import backgroundImage from "../assets/welcome-image.jpg";
+import backgroundImage2 from "../assets/welcome-image-3.jpg";
+import screens from "../assets/mobilescreens.png";
+
 export default {
-    data() {
-        return {
-            title: 'batman'
-        }
-    },
-}
+  data() {
+    return {
+      image: backgroundImage,
+      image2: backgroundImage2,
+      screens: screens,
+      title: "shotties",
+    };
+  },
+};
 </script>
 
 <style scoped>
+.welcome {
+  width: 100%;
+  height: 100vh;
+  position: relative;
 
+  padding-top: 15vh;
+}
+.background-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -1;
+}
+
+.title {
+  text-align: center;
+  font-size: 5rem;
+  color: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  margin: 1rem auto;
+  padding: 1rem;
+  width: 40rem;
+  font-family: "Audiowide", cursive;
+  border-bottom: dotted 2px white;
+}
+
+.screens-wrap {
+
+    position: absolute;
+    bottom: 0;
+    background: rgb(255, 255, 255);
+    width: 100%;
+    height: 40vh;
+    padding: 1rem 20rem;
+}
+.mobile-screens {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  
+}
 </style>
