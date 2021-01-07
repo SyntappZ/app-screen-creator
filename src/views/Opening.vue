a75775a58087f04348bfe448c286b5167b95c281
syntappz <doddsanater87@gmail.com>
2021-01-07 15:37:06 +0000

    main router setup

---
+++ src/views/Opening.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="opening-page">
+    <Welcome /> 
+  </div>
+</template>
+
+<script>
+import Welcome from "../components/Welcome.vue";
+
+export default {
+  name: "Opening",
+  components: {
+    Welcome,
+  },
+};
+</script>
